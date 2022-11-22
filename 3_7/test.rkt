f37618d2e0731f26ca4d31db3238e4f53d20cdb0
Denis Manikhin <dmanikhine@gmail.com>
2022-11-22 11:30:46 +0100

    add test for 3_7

--- 3_7/test.rkt
+++ 3_7/test.rkt
@@ -34,14 +34,28 @@
 
 (define (make-joint acc acc-password joint-password)
   (if (acc acc-password 'test-password) 
-      (make-passworded (acc acc-password) joint-password)
-      (display "incorrect password")))
+      (begin (display "Add new user for main account") (make-passworded (acc acc-password) joint-password))
+      (display "Can't add new user, incorrect password for main account!")))
 
 (define acc-aisha
-  (make-joint acc-denis 'secret 'top-secret))
+  (make-joint acc-denis 'secret 'aisha-secret))
+(newline)
 
-(((acc-aisha 'top-secret) 'deposit) 100)
+(((acc-aisha 'aisha-secret) 'deposit) 100)
 
 (((acc-denis 'secret) 'deposit) 20)
 
-(((acc-aisha 'top-secret) 'deposit) 100)
+(((acc-aisha 'aisha-secret) 'deposit) 100)
+
+(define acc-oxana
+  (make-joint acc-denis 'oxana 'oxana-secret))
+
+(newline)
+
+(define acc-victoria
+  (make-joint acc-aisha 'aisha-secret 'victoria-secret))
+(newline)
+
+(((acc-victoria 'victoria-secret) 'withdraw) 200)
+
+(((acc-denis 'secret) 'deposit) 0)
