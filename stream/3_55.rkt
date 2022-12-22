93c826c9c0b89524fccf7fdc53618abf0cdddcc2
Denis Manikhin <dmanikhine@gmail.com>
2022-12-22 14:47:24 +0100

    add 3-55 versions 2

--- stream/3_55.rkt
+++ stream/3_55.rkt
@@ -14,16 +14,25 @@
     (cons-stream (stream-car s)
                (add-streams (partial-sums-v1 s) (stream-cdr s))))
 
+(define (partial-sums-v2 s) 
+   (add-streams s (cons-stream 0 (partial-sums-v2 s)))) 
 
 
+(define stream-test-v1 (partial-sums-v1 integers))
+
+(stream-ref stream-test-v1 0)
+(stream-ref stream-test-v1 1)
+(stream-ref stream-test-v1 2)
+(stream-ref stream-test-v1 3)
+(stream-ref stream-test-v1 4)
 
 
+(define stream-test-v2 (partial-sums-v2 integers))
 
-(define stream-test (partial-sums-v1 integers))
+(stream-ref stream-test-v2 0)
+(stream-ref stream-test-v2 1)
+(stream-ref stream-test-v2 2)
+(stream-ref stream-test-v2 3)
+(stream-ref stream-test-v2 4)
 
-(stream-ref stream-test 0)
-(stream-ref stream-test 1)
-(stream-ref stream-test 2)
-(stream-ref stream-test 3)
-(stream-ref stream-test 4)
 
